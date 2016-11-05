9f0de4c3024fe752af1804aed85b9929af183bd2
fengyifeng <fengyifeng1104@126.com>
2016-11-05 12:28:16 +0800

    comiit

---
+++ src/main/java/com/ymatou/uitest/App.java
@@ -0,0 +1,21 @@
+package com.ymatou.uitest;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import com.google.common.collect.ImmutableMap;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        ImmutableMap.of("using", "d", "value", "d");
+        WebDriver webDriver = new ChromeDriver();
+        
+    }
+}
