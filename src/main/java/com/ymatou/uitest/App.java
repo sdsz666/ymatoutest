1d32e3324001404fb3672d6822ff576d47111340
fengyifeng1104 <fengyifeng1104@126.com>
2016-11-05 13:20:45 +0800

    Update App.java

--- src/main/java/com/ymatou/uitest/App.java
+++ src/main/java/com/ymatou/uitest/App.java
@@ -14,7 +14,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        ImmutableMap.of("using", "d", "value", "d");
         
     }
 }
